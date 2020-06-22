13821b7731f8b84adce6836dc40a7fa8fa965ebf
Ana87sim <anasimoes@MBP-de-Ana-4.lan>
2020-06-22 14:12:14 +0100

    added icons

--- src/index.js
+++ src/index.js
@@ -81,10 +81,23 @@ celsiusSelector.addEventListener("click", convertToCelsius);
 let buttonCurrentLocation = document.querySelector("#button-link");
 
 function showTemperature(response) {
-  let city = document.querySelector("#city-display");
-  let temperature = document.querySelector("#temperature");
-  city.innerHTML = response.data.name;
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
+  let cityElement = document.querySelector("#city-display");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function retrievePosition(position) {
@@ -105,13 +118,33 @@ buttonCurrentLocation.addEventListener("click", getPosition);
 let cityForm = document.querySelector("#city-form");
 
 function searchCity(event) {
+  console.log(cityInput.value);
   event.preventDefault();
-
   let cityInput = document.querySelector("#city-input");
   let apiKey = "9bc8a95bb24bea24e1011758969f01a6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
-  console.log(cityInput.value);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 cityForm.addEventListener("submit", searchCity);
+
+/*
+function displayForecast(response) {
+  let forecastweekdayElement = document.querySelector("#forecast-weekday");
+  let forecastdateElement = document.querySelector("#forecast-date");
+  let forecasttempElement = document.querySelector("#forecast-temp");
+  let forecasticonElement = document.querySelector("#forecast-icon");
+
+  forecastweekdayElement.innerHTML = null;
+  forecastdateElement.innerHTML = null;
+  forecasttempElement.innerHTML = null;
+  forecasticonElement.innerHTML = null;
+
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += */
