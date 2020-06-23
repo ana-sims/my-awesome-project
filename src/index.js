0121e6e0052648053b68e1a3dbebc397f371f0b6
Ana87sim <anasimoes@MBP-de-Ana-4.lan>
2020-06-23 14:58:47 +0100

    implemented forecast

--- src/index.js
+++ src/index.js
@@ -40,8 +40,12 @@ let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 
-time.innerHTML = `${now.getHours()}:${minutes}`;
+time.innerHTML = `${hours}:${minutes}`;
 
 //Unit Conversion
 
@@ -112,7 +116,19 @@ buttonCurrentLocation.addEventListener("click", getPosition);
 
 let cityForm = document.querySelector("#city-form");
 
-function formatHours() {}
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
